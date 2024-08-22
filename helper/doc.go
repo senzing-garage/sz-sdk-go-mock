d25d94677acde10c9129e9473a2edc9ef7440d98
docktermj <michael@dockter.com>
2024-08-22 14:05:13 -0400

    #146 Update documentation

---
+++ helper/doc.go
@@ -0,0 +1,12 @@
+/*
+Package helper is not intended for public use.
+It contains functions that are common to the
+[szconfig], [szconfigmanager], [szdiagnostic], [szengine], and  [szproduct] packages.
+
+[szconfig]: https://pkg.go.dev/github.com/senzing-garage/sz-sdk-go-mock/szconfig
+[szconfigmanager]: https://pkg.go.dev/github.com/senzing-garage/sz-sdk-go-mock/szconfigmanager
+[szdiagnostic]: https://pkg.go.dev/github.com/senzing-garage/sz-sdk-go-mock/szdiagnostic
+[szengine]: https://pkg.go.dev/github.com/senzing-garage/sz-sdk-go-mock/szengine
+[szproduct]: https://pkg.go.dev/github.com/senzing-garage/sz-sdk-go-mock/szproduct
+*/
+package helper
--- szconfigmanager/doc.go
+++ szconfigmanager/doc.go
@@ -1,4 +1,4 @@
 /*
-The szconfigmanager package is used make SzConfigmgr requests to a mock object.
+Package szconfigmanager is used to modify Senzing configuration JSON documents in the Senzing datastore.
 */
 package szconfigmanager
