37331ae9ceca7590e879b7de51b56fcf6a6e6e38
docktermj <michael@dockter.com>
2025-02-07 14:11:38 -0500

    #178 Savepoint

--- testdata/testdata_test.go
+++ testdata/testdata_test.go
@@ -1,20 +1,32 @@
 package testdata
 
-import (
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-)
-
 // ----------------------------------------------------------------------------
 // Interface methods - test
 // ----------------------------------------------------------------------------
 
-func TestTestdata_String(test *testing.T) {
-	testValue := &TestData{
-		Int64s:   Data1_int64s,
-		Strings:  Data1_strings,
-		Uintptrs: Data1_uintptrs,
-	}
-	assert.Equal(test, "", testValue.String("GetFeatureResult"))
-}
+// func TestDestdata_Int64(test *testing.T) {
+// 	testValue := &TestData{
+// 		Int64s:   Data1_int64s,
+// 		Strings:  Data1_strings,
+// 		Uintptrs: Data1_uintptrs,
+// 	}
+// 	assert.Equal(test, int64(0), testValue.Int64("GetFeatureResult"))
+// }
+
+// func TestDestdata_String(test *testing.T) {
+// 	testValue := &TestData{
+// 		Int64s:   Data1_int64s,
+// 		Strings:  Data1_strings,
+// 		Uintptrs: Data1_uintptrs,
+// 	}
+// 	assert.Equal(test, "", testValue.String("GetFeatureResult"))
+// }
+
+// func TestDestdata_Uintptr(test *testing.T) {
+// 	testValue := &TestData{
+// 		Int64s:   Data1_int64s,
+// 		Strings:  Data1_strings,
+// 		Uintptrs: Data1_uintptrs,
+// 	}
+// 	assert.Equal(test, uintptr(99), testValue.Uintptr("GetFeatureResult"))
+// }
